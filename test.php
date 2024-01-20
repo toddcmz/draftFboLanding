9d7fee1dd19d295c1f93e8928cc288b00f451a84
toddcmz <toddcmz@gmail.com>
2024-01-20 12:08:18 -0700

    drafting course and workshops details section

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $to = "toddcmz@gmail.com"; // <– replace with your address here
-  $subject = "testMail";
-  $message = "Hello, this is a test message.";
-  $from = "todd@devtlc.co";
-  $headers = ['From' => $from, 'Reply-To' => $from, 'Content-type' => 'text/html; charset=utf-8'];
-  mail($to,$subject,$message,$headers);
-  echo "Mail Sent.";
-?>
